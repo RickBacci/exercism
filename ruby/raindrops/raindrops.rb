580071e369e6a54e2bc7a1619285a3ab7f2122bc
Richard Bacci <brownieelf175@gmail.com>
2015-04-06 08:16:29 -0600

    fix inject method, remove unneeded varialbe

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,22 +1,19 @@
 
 class Raindrops
 
-
     @data = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }
-
 
   def self.has_no_prime_factor?(number)
     @data.keys.each { |prime| return false if number % prime == 0 }
   end
 
   def self.convert(number)
-    mt_string = ''
 
     return number.to_s if has_no_prime_factor?(number)
 
-    @data.inject('') do |rain_drops, array| # [3, 'blah']
-      mt_string << array[1] if number % array[0] == 0
+    @data.inject('') do |rain_drops, array|
+      rain_drops << array[1] if number % array[0] == 0
+      rain_drops
     end
-    mt_string
   end
 end
